9ff090142f35084a3bb0b7f070974fae863f35ab
snehalminal <50071405+snehalminal@users.noreply.github.com>
2019-04-28 15:28:42 +0530

    Add files via upload

---
+++ arraydemo.js
@@ -0,0 +1,11 @@
+var m1=require("./functionmodule");
+//var arr=new Array(25,30,40,10);
+var arr1=Array(23,54,64,74);
+//console.log(arr.length);
+//console.log(arr1.length);
+var sum=m1.add(arr1);
+console.log("sum is:"+sum);
+
+/* var key=10;
+var pos=m1.search(arr,key);
+console.log("value found at"+pos+"position"); */
